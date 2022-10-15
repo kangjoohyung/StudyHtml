4e317351ccec74931f74b06b75139ec545c37d14
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-15 12:26:07 +0900

    221015 변수이름 수정

--- src/main/java/ex221015/Ex02InitParamServlet.java
+++ src/main/java/ex221015/Ex02InitParamServlet.java
@@ -9,26 +9,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Ex02InitParamServlet extends HttpServlet {
-	private String dbId, dbPwd , message, driverName;
+	private String ex221015dbId, ex221015dbPwd , ex221015message, ex221015driverName;
 	@Override
 	public void init() throws ServletException {
        ServletContext application = super.getServletContext();
-		message = application.getInitParameter("message");
-		driverName = application.getInitParameter("driverName");
+		ex221015message = application.getInitParameter("ex221015message");
+		ex221015driverName = application.getInitParameter("ex221015driverName");
 		
-		dbId = super.getInitParameter("dbId");
-		dbPwd = getInitParameter("dbPwd");
+		ex221015dbId = super.getInitParameter("ex221015dbId");
+		ex221015dbPwd = getInitParameter("ex221015dbPwd");
 		
-		System.out.println("dbId = " + dbId +" , dbPwd = " + dbPwd);
-		System.out.println("message = " + message +" , driverName = " + driverName);
+		System.out.println("ex221015dbId = " + ex221015dbId +" , ex221015dbPwd = " + ex221015dbPwd);
+		System.out.println("ex221015message = " + ex221015message +" , ex221015driverName = " + ex221015driverName);
 		
 	}
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//
-		System.out.println("Service call ==>  dbId = " + dbId +" , dbPwd = " + dbPwd);
-		System.out.println("Service call ==>  message = " + message +" , driverName = " + driverName);
+		System.out.println("Service call ==>  ex221015dbId = " + ex221015dbId +" , ex221015dbPwd = " + ex221015dbPwd);
+		System.out.println("Service call ==>  ex221015message = " + ex221015message +" , ex221015driverName = " + ex221015driverName);
 	}
 
 }
