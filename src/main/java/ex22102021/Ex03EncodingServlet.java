7261525ec4cf11d36e20bc84380586a85a04e347
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-21 14:47:14 +0900

    221021 수정4

--- src/main/java/ex22102021/Ex03EncodingServlet.java
+++ src/main/java/ex22102021/Ex03EncodingServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EncodingServlet
  */
-@WebServlet("/encodingTest")
+@WebServlet("/Ex03EncodingTest")
 public class Ex03EncodingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
