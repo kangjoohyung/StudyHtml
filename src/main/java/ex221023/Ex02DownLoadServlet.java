e94b00f02a9355c2e2c424dcdac1bff92b0a5f9c
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-24 13:32:21 +0900

    221024 수정3

--- src/main/java/ex221023/Ex02DownLoadServlet.java
+++ src/main/java/ex221023/Ex02DownLoadServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/downLoad")
+@WebServlet("/ex221023Ex02DownLoad")
 public class Ex02DownLoadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
