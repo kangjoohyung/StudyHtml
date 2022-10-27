1adbd049f3e24b93264593ecf3b3ff6e54b3f749
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-27 12:43:44 +0900

    221027 수정5

--- src/main/java/ex22102526/Ex10InsertServlet.java
+++ src/main/java/ex22102526/Ex10InsertServlet.java
@@ -7,21 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kosta.dao.MemberDAO;
-import kosta.dao.MemberDAOImpl;
-import kosta.dto.Member;
 
 /**
  * Servlet implementation class InsertServlet
  */
-@WebServlet("/insert")
+@WebServlet("/22102526insert")
 public class Ex10InsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		//변수 선언
-		String url="error.jsp";
+		String url="SelfStudy/22102526/03_error.jsp";
 		String errMsg="등록되지 않았습니다";//에러메세지
 		boolean isRedirect=false; //true이면 redirect방식, false이면 forward방식으로 할 예정
 		
@@ -41,12 +38,12 @@ public class Ex10InsertServlet extends HttpServlet {
 			errMsg="입력값이 충분하지 않습니다. \\n다시 확인해주세요."; // \n 만쓰면 script에러남
 		}else {
 			//중복체크
-			MemberDAO dao=new MemberDAOImpl();
+			Ex05MemberDAO dao=new Ex06MemberDAOImpl();
 			if(dao.duplicateCheckById(id)) {
 				errMsg=id+"는 이미 사용중입니다";
 			}else {
 				//등록하기
-				Member member=new Member(id, pwd, name, Integer.parseInt(age), phone, addr, null);
+				Ex04Member member=new Ex04Member(id, pwd, name, Integer.parseInt(age), phone, addr, null);
 				if(dao.insert(member)>0) {//등록 성공했을때
 					url="selectAll";//서블릿
 					isRedirect=true; //초기값false에서 성공시 true로 변경
