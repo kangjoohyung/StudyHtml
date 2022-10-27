71d54c597ea80c931dd58619ab2d65553ca2b603
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-27 12:44:26 +0900

    221027 수정6

--- src/main/java/ex22102526/Ex11DeleteServlet.java
+++ src/main/java/ex22102526/Ex11DeleteServlet.java
@@ -13,18 +13,18 @@ import kosta.dao.MemberDAOImpl;
 /**
  * Servlet implementation class DeleteServlet
  */
-@WebServlet("/delete")
+@WebServlet("/22102526delete")
 public class Ex11DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id=request.getParameter("id");
 		
-		MemberDAO dao=new MemberDAOImpl();
+		Ex05MemberDAO dao=new Ex06MemberDAOImpl();
 		if(dao.delete(id)==0) {
 			//삭제안됨 -> 메세지 송출, 에러페이지로 넘어감
 			request.setAttribute("errMsg", id+"정보를 삭제할 수 없습니다");
-			request.getRequestDispatcher("error.jsp").forward(request, response);
+			request.getRequestDispatcher("SelfStudy/22102526/03_error.jsp").forward(request, response);
 		}else {
 			//삭제됨 -> 다시 검색하러
 			response.sendRedirect("selectAll");//삭제되어 정보갱신겸 서블릿 response로
