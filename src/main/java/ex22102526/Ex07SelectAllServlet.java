01c7a509acf994d2988e88874cd110b87f2d1de4
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-27 12:38:10 +0900

    221027 수정2

--- src/main/java/ex22102526/Ex07SelectAllServlet.java
+++ src/main/java/ex22102526/Ex07SelectAllServlet.java
@@ -9,20 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kosta.dao.MemberDAO;
-import kosta.dao.MemberDAOImpl;
-import kosta.dto.Member;
 
 /**
  * Servlet implementation class SelectAllServlet
  */
-@WebServlet("/selectAll")
+@WebServlet("/22102526selectAll")
 public class Ex07SelectAllServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		MemberDAO dao=new MemberDAOImpl(); //DAO로 넘기기위해 호출
-		List<Member> list=dao.selectAll(); //List에 DTO담기
+		Ex05MemberDAO dao=new Ex06MemberDAOImpl(); //DAO로 넘기기위해 호출
+		List<Ex04Member> list=dao.selectAll(); //List에 DTO담기
 		
 		//list를 scope영역에 저장
 		request.setAttribute("memberList", list); //키,밸류 저장 -> 뷰에서 키 : ${memberList} 호출
