1a9a73b05bc0fc96fa7341387cedc61e7518994d
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-30 02:13:35 +0900

    221030 수정2

--- src/main/java/ex22102930/Ex04InsertController.java
+++ src/main/java/ex22102930/Ex04InsertController.java
@@ -6,15 +6,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Ex04InsertController implements Controller {
+public class Ex04InsertController implements Ex02Controller {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex03ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		System.out.println("InsertController 호출");//확인용 콘솔송출
 		
 		// service호출-->dao호출-->등록이 완료되면 redirect방식으로 전체검색으로 이동
-		ModelAndView mv=new ModelAndView("front?key=select", true);
+		Ex03ModelAndView mv=new Ex03ModelAndView("22102930front?key=select", true);
 		
 		return mv;
 	}
