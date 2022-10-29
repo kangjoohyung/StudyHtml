3c4dce03f42e85d3a826dfe316630877e1ce3fff
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-30 02:14:43 +0900

    221030 수정4

--- src/main/java/ex22102930/Ex06SelectController.java
+++ src/main/java/ex22102930/Ex06SelectController.java
@@ -6,17 +6,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Ex06SelectController implements Controller {
+public class Ex06SelectController implements Ex02Controller {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex03ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		System.out.println("SelectController 호출");
 		// service호출 --> dao호출해서 --> 검색결과를 영속성에 저장하고 결과뷰로 이동한다.
 		
 		request.setAttribute("message", "전체검색 결과입니다");//${message} 뷰 사용
 		
-		ModelAndView mv=new ModelAndView("selectResult.jsp");//
+		Ex03ModelAndView mv=new Ex03ModelAndView("selectResult.jsp");//
 		return mv;
 	}
 
