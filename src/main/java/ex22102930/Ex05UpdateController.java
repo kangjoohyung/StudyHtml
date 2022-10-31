331e17b5fb9afd6e2f9f34784030b15d83601cfa
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-10-31 23:23:45 +0900

    221031 수정2

--- src/main/java/ex22102930/Ex05UpdateController.java
+++ src/main/java/ex22102930/Ex05UpdateController.java
@@ -15,6 +15,6 @@ public class Ex05UpdateController implements Ex02Controller {
 		// service호출-->dao 수정완료 결과뷰로 이동
 		request.setAttribute("message", "수정이 완료되었습니다");
 		
-		return new Ex03ModelAndView("03_updateResult.jsp"); // 변수없이 바로 생성해서 리턴해봄
+		return new Ex03ModelAndView("SelfStudy/22102930/03_updateResult.jsp"); // 변수없이 바로 생성해서 리턴해봄
 	}
 }
