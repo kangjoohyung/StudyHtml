7aa5b4746d5a06aef6f3613645ce5a6b1a3f1af6
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-11-02 22:17:30 +0900

    221102 생성8

--- src/main/java/ex221101/Ex09DeleteController.java
+++ src/main/java/ex221101/Ex09DeleteController.java
@@ -6,16 +6,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Ex09DeleteController implements Controller {
+public class Ex09DeleteController implements Ex03Controller {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex05ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		System.out.println("DeleteController호출");
 		// service->dao호출해서 삭제완료된 후 이동
 		//이번엔 forward방식으로, 메인화면으로 이동시켜봄
-		ModelAndView mv=new ModelAndView();
-		mv.setViewName("index.jsp");
+		Ex05ModelAndView mv=new Ex05ModelAndView();
+		mv.setViewName("SelfStudy/221031/01_index.jsp");
 		mv.setRedirect(true);
 		
 		return mv;
