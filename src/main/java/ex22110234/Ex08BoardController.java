8afc641d3533de4a49e1d1f4b198074c1d2c9ede
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-11-06 17:57:28 +0900

    221106 수정1

--- src/main/java/ex22110234/Ex08BoardController.java
+++ src/main/java/ex22110234/Ex08BoardController.java
@@ -9,10 +9,10 @@ import javax.servlet.http.HttpServletResponse;
  * 게시판에 관련된 Controller
  *
  */
-public class Ex08BoardController implements Controller {
+public class Ex08BoardController implements Ex03Controller {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex06ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		return null;
@@ -21,21 +21,21 @@ public class Ex08BoardController implements Controller {
 	/**
 	 * 게시물 검색
 	 */
-	public ModelAndView list(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex06ModelAndView list(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		System.out.println("BoardController의 list");
 		
 		request.setAttribute("message", "검색결과입니다");
-		return new ModelAndView("board/list.jsp");
+		return new Ex06ModelAndView("22110234/06_list.jsp");
 	}
 	
 	/**
 	 * 상세보기
 	 */
-	public ModelAndView read(HttpServletRequest request, HttpServletResponse reponse)
+	public Ex06ModelAndView read(HttpServletRequest request, HttpServletResponse reponse)
 			throws ServletException, IOException {
 		System.out.println("BoardController의 read");
 		
-		return new ModelAndView("board/read.jsp", true);
+		return new Ex06ModelAndView("22110234/07read.jsp", true);
 	}
 }
