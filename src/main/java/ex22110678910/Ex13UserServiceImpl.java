672a9c6152b036f08559402eda67fe6b4de361fd
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-11-09 13:53:34 +0900

    221109 생성8

---
+++ src/main/java/ex22110678910/Ex13UserServiceImpl.java
@@ -0,0 +1,30 @@
+package ex22110678910;
+
+import java.sql.SQLException;
+
+import kosta.mvc.dao.UserDAO;
+import kosta.mvc.dao.UserDAOImpl;
+import kosta.mvc.dto.UserDTO;
+import kosta.mvc.exception.AuthenticationException;
+
+public class Ex13UserServiceImpl implements UserService {
+     private UserDAO userDAO = new UserDAOImpl();
+  	
+	@Override
+	public UserDTO loginCheck(UserDTO userDTO) throws SQLException, AuthenticationException {
+		
+		//dao호출
+		UserDTO dbDTO = userDAO.loginCheck(userDTO);
+		if(dbDTO == null) {
+			throw new AuthenticationException("정보를 다시 확인해주세요.");
+		}
+		
+		return dbDTO;
+	}
+
+}
+
+
+
+
+
