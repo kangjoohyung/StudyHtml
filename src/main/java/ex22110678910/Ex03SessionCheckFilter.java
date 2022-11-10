e0be5b30bc3ff75799b3130e774c54fa55ab0ea8
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-11-10 19:05:04 +0900

    221110 수정3

--- src/main/java/ex22110678910/Ex03SessionCheckFilter.java
+++ src/main/java/ex22110678910/Ex03SessionCheckFilter.java
@@ -27,14 +27,14 @@ public class Ex03SessionCheckFilter implements Filter {
 		System.out.println("SessionCheck....");
 		//사전처리 -> 인증여부를 체크한다.
 		String key = request.getParameter("key");
-		if(key==null || key.equals("elec")) {//이곳에 진입하는 key 넣기-Dispatcher서블릿에 입력된 그대로
+		if(key==null || key.equals("22110678910elec")) {//이곳에 진입하는 key 넣기-Dispatcher서블릿에 입력된 그대로
 			
 			//인증된 사용자만 해라...
 			HttpServletRequest req = (HttpServletRequest)request;
 			HttpSession session = req.getSession();
 			if(session.getAttribute("loginUser") ==null) {
 				req.setAttribute("errorMsg", "로그인하고 이용해주세요.^^");
-				req.getRequestDispatcher("error/error.jsp").forward(request, response);
+				req.getRequestDispatcher("SelfStudy/22110678910_important/05error.jsp").forward(request, response);
 				return ;//함수를 빠져나가라
 			}
 		}
